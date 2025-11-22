63054372bf2392b483b870d9f179303bcbee10d2
Alisher <123088556+Alisher9999@users.noreply.github.com>
2025-11-22 19:06:36 +0500

    Add files via upload

---
+++ Game Space Wars (Version 1)/main.py
@@ -0,0 +1,26 @@
+# Game Space Wars (Version 1) in Python
+
+import pygame, controls
+from gun import Gun
+from pygame.sprite import Group
+
+def run():
+
+    pygame.init()
+    screen = pygame.display.set_mode((700, 800))
+    pygame.display.set_caption("Space wars")
+    bg_color = (0, 0, 0)
+    gun = Gun(screen)
+    bullets = Group()
+    inos = Group()
+    controls.create_army(screen, inos)
+
+    while True:
+        controls.events(screen, gun, bullets)
+        gun.update_gun()
+        controls.update(bg_color, screen, gun, inos, bullets)
+        controls.update_bullets(inos, bullets)
+        controls.update_inos(inos)
+
+
+run()
